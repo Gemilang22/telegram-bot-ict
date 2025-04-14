81dd0e7768bd0a0d0fa1bde1a35aee9b42300b25
Gemilang22 <136076210+Gemilang22@users.noreply.github.com>
2025-04-15 04:30:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-import logging
+import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.utils import executor
+from aiogram.enums import ParseMode
+from aiogram.types import Message
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram import Router
 
-API_TOKEN = '7830689776:AAFJabHa7QdnuKfz0b97N8x5TGsl9RPPBX0'
+API_TOKEN = "7830689776:AAFJabHa7QdnuKfz0b97N8x5TGsl9RPPBX0"
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
+router = Router()
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+@router.message(commands=["start"])
+async def cmd_start(message: Message):
+    await message.answer("Welcome to the bot!")
 
-# Register a message handler
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Welcome to the bot!")
+async def main():
+    bot = Bot(token=API_TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
 
-# This function runs the bot
-async def on_start():
-    await dp.start_polling()
+    await dp.start_polling(bot)
 
-if __name__ == '__main__':
-    # Start the bot
-    executor.start_polling(dp, skip_updates=True)
+if __name__ == "__main__":
+    asyncio.run(main())
